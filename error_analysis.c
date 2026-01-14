02d1f72002a2b1e64d5f8f67e710eed87d868f30
tushar0289 <190056372+tushar0289@users.noreply.github.com>
2026-01-15 00:49:12 +0600

    Add error analysis code, output data and image

---
+++ error_analysis.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+    FILE *fref = fopen("reference.txt", "r");
+    FILE *fftcs = fopen("ref_ftcs(0.1).dat", "r");
+    FILE *fcn = fopen("ref_crank_nicolson(0.1).txt", "r");
+    FILE *f_error = fopen("error_analysis.txt", "w");
+    
+    double x, u_ref, u_ftcs, u_cn;
+    
+    fprintf(f_error, "x ftcs_error cn_error");
+    
+    while(fscanf(fref, "%lf %lf", &x, &u_ref) != EOF && fscanf(fref, "%*f %lf", &u_ftcs) != EOF && fscanf(fcn, "%*f %lf", &u_cn) != EOF){
+        double err_ftcs = fabs(u_ftcs - u_ref);
+        double err_cn = fabs(u_cn - u_ref);
+        
+        fprintf(f_error, "%8g %12e %12e\n", x, err_ftcs, err_cn);
+    }
+    
+    
+    fclose(fref);
+    fclose(fftcs);
+    fclose(fcn);
+    fclose(f_error);
+    return 0;
+}
